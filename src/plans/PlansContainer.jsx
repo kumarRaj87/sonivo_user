c4684b11ce716f988e213b1c49f7fb2f28dd8a26
KumarRaj21 <konnakumarraja097@gmail.com>
2025-04-14 18:04:22 +0530

    added loader

--- src/plans/PlansContainer.jsx
+++ src/plans/PlansContainer.jsx
@@ -1,13 +1,6 @@
-
-
 import React, { useState, useEffect } from "react";
 import PlanCard from "./PlanCard";
-
-const Loader = () => (
-  <div className="flex items-center justify-center min-h-[50vh]">
-    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary-500"></div>
-  </div>
-);
+import Loader from "../components/loader/Loader";
 
 const PlansContainer = () => {
   const [plans, setPlans] = useState([]);
