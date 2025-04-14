0bc6ae08ca879fffc7c6ddd5077b6bd660275588
KumarRaj21 <konnakumarraja097@gmail.com>
2025-04-14 13:37:01 +0530

    added sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -12,14 +12,14 @@ import {
     Contact,
     ArrowDownUp
 } from 'lucide-react'
- 
+
 import { TbArrowFork } from "react-icons/tb";
 import { TbPhoneRinging } from "react-icons/tb";
 import { PiPhonePlusBold } from "react-icons/pi";
- 
+
 const Sidebar = () => {
     const location = useLocation()
- 
+
     const menuItems = [
         { divider: true, label: 'Useful' },
         { path: '/dashboard', icon: MdSpaceDashboard, label: 'Dashboard' },
@@ -43,20 +43,20 @@ const Sidebar = () => {
         { path: '/device-manager', icon: PiPhonePlusBold, label: 'Device Manager' }
 
     ]
- 
+
     const isActive = (path) => {
         return location.pathname === path
     }
- 
+
     return (
         <aside className={`fixed hidden lg:block inset-y-0 left-0 w-60 bg-background transform transition-transform duration-300 ease-in-out`}>
-             <div className="py-5 flex items-center px-4">
-          <Link to="/dashboard" className="flex items-center space-x-2 pl-2">
-            <span className="text-2xl font-semibold text-[#1C2833]">Vokal</span>
-          </Link>
-        </div>
+            <div className="py-5 flex items-center px-4">
+                <Link to="/dashboard" className="flex items-center space-x-2 pl-2">
+                    <span className="text-2xl font-semibold text-[#1C2833]">Vokal</span>
+                </Link>
+            </div>
 
- 
+
             <nav className="px-3 h-[88vh] overflow-y-auto pb-2">
                 {menuItems.map((item, index) => (
                     item.divider ? (
@@ -84,5 +84,5 @@ const Sidebar = () => {
         </aside>
     )
 }
- 
+
 export default Sidebar
