a1bb158540c64335b90f75ca01570fb66e856ae0
KumarRaj21 <konnakumarraja097@gmail.com>
2025-04-14 13:01:38 +0530

    added plan card

--- src/plans/PlansContainer.jsx
+++ src/plans/PlansContainer.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import PlanCard from "./PlanCard";
-import { IoMdPhonePortrait } from "react-icons/io";
 import Loader from "../components/loader/Loader";
 
 const PlansContainer = () => {
